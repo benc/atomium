1808aad1532356cf4dfac8e12de89ae798edebba
Ben Cochez <ben@cochez.consulting>
2020-06-25 13:29:20 +0200

    attempt at using a generator
    does not work, generator seems to fire too often

--- modules/client-java/src/main/java/be/wegenenverkeer/atomium/client/AtomiumFeed.java
+++ modules/client-java/src/main/java/be/wegenenverkeer/atomium/client/AtomiumFeed.java
@@ -1,17 +1,21 @@
 package be.wegenenverkeer.atomium.client;
 
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Emitter;
 import io.reactivex.rxjava3.core.Flowable;
 import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.functions.BiConsumer;
+import io.reactivex.rxjava3.functions.Consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.awt.*;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class AtomiumFeed<E> {
     private static String FIRST_PAGE = "/";
+    public static final FeedPosition HEAD_PAGE_FEED_POSITION = FeedPositions.of(AtomiumFeed.FIRST_PAGE);
     private final static Logger logger = LoggerFactory.getLogger(AtomiumFeed.class);
     private final PageFetcher<E> pageFetcher;
 
@@ -20,37 +24,44 @@ public class AtomiumFeed<E> {
     }
 
     private int retryCount = 0;
+    private AtomicReference<CachedFeedPage<E>> generatorState = new AtomicReference<>();
 
     public Flowable<FeedEntry<E>> fetchEntries(FeedPositionStrategy feedPositionStrategy) {
-        // recursion breaks backpressure, so we're using a generator:
-        AtomicReference<CachedFeedPage<E>> previousPage = new AtomicReference<>(null);
-        return Flowable.generate( // generate an infinite stream of zeroes (Flowable.range only goes to Integer.MAX_VALUE)
-                () -> 0,
-                (s, emitter) -> {
-                    emitter.onNext(0);
-                }
-            ).concatMap(t -> { // fetch the page to pass to the feed position strategy
-                    Single<CachedFeedPage<E>> page;
-                    if (previousPage.get() != null) {
-                        page = Single.just(previousPage.get());
-                    } else {
-                        page = fetchHeadPage();
-                    }
-                    return page.flatMap(eCachedFeedPage -> feedPositionStrategy.getNextFeedPosition(eCachedFeedPage) // get next feed position
-                                    .flatMap(feedPosition -> fetchPage(feedPosition.pageUrl, eCachedFeedPage.etag) // fetch entries, and parse them
-                                            .doOnSuccess(previousPage::set)
-                                            .map(cachedFeedPage -> ParsedFeedPage.parse(cachedFeedPage, feedPosition))))
-                            .toFlowable()
-                            .flatMap(parsedPage -> Flowable.fromIterable(parsedPage.getEntries()));
-                }, 1);
+        return fetchHeadPage().toFlowable().concatMap(headPage -> fetchEntries(headPage, feedPositionStrategy));
+    }
+
+    private Flowable<FeedEntry<E>> fetchEntries(CachedFeedPage<E> initialPage, FeedPositionStrategy feedPositionStrategy) {
+        generatorState.set(initialPage);
+
+        return Flowable.generate(getNextPage(feedPositionStrategy))
+                .concatMap(pageRef -> pageRef
+                        .toFlowable()
+                        .doOnNext(nextPage -> logger.debug("Parsing page {}", nextPage.getSelfHref()))
+                        .map(nextPage -> ParsedFeedPage.parse(nextPage, FeedPositions.of(nextPage.getSelfHref())))// TODO fix, we should return a tuple of page and feed position
+                        .flatMap(parsedPage -> Flowable.fromIterable(parsedPage.getEntries()))
+                        .doOnNext(eFeedEntry -> logger.debug("Emitting feed entry {} on page {}", eFeedEntry.getEntry().getId(), eFeedEntry.getSelfHref())
+                        ), 1);
+    }
+
+    private Consumer<Emitter<Single<CachedFeedPage<E>>>> getNextPage(FeedPositionStrategy feedPositionStrategy) {
+        return (feedEntryEmitter) -> {
+            logger.debug("Generator fired, calculating next page..."); // dit gebeurt niet in volgorde...
+            Single<CachedFeedPage<E>> nextPage = feedPositionStrategy.getNextFeedPosition(generatorState.get())
+                    .doOnSuccess(nextFeedPos -> logger.debug("Next page will be {}", nextFeedPos.getPageUrl()))
+                    .concatMap(this::fetchPage)
+                    .doOnSuccess(fetchedPage -> logger.debug("Page {} fetched!", fetchedPage.getSelfHref()))
+                    .doOnSuccess(fetchedPage -> generatorState.set(fetchedPage));
+            logger.debug("Emitting value..."); // dit gebeurt niet in volgorde...
+            feedEntryEmitter.onNext(nextPage);
+        };
     }
 
     private Single<CachedFeedPage<E>> fetchHeadPage() {
-        return fetchPage(AtomiumFeed.FIRST_PAGE, Optional.empty());
+        return fetchPage(HEAD_PAGE_FEED_POSITION);
     }
 
-    private Single<CachedFeedPage<E>> fetchPage(String pageUrl, Optional<String> eTag) {
-        return pageFetcher.fetch(pageUrl, eTag)
+    private Single<CachedFeedPage<E>> fetchPage(FeedPosition feedPosition) {
+        return pageFetcher.fetch(feedPosition.getPageUrl(), Optional.empty())
                 .retryWhen(throwableFlowable -> throwableFlowable
                         .flatMap(this::applyRetryStrategy)
                         .flatMap(delay -> Flowable.just("ignored").delay(delay.longValue(), TimeUnit.MILLISECONDS))
